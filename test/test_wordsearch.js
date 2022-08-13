327bf7e50124d93450b4637e0014bbefc969a206
AnhLe3698 <anhviet.le00@hotmail.com>
2022-08-13 15:43:07 -0600

    finished everything including the stretch

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,10 +1,10 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word is not present - FRANK is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present - Seinfeld is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,101 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+
+  it("should return false if the word is present - SINK not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SINK');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is not present - FRANK verticall present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'S', 'A', 'N', 'K'],
+      ['Y', 'F', 'C', 'R', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'A', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'K', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+
+    assert.isTrue(result);
+  });
+
+
+  it("should return false if the word is falsy value, emply string returns false", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'S', 'A', 'N', 'K'],
+      ['Y', 'F', 'C', 'R', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'A', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'K', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is falsy value, AFJ can be found Diagonally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'S', 'A', 'N', 'K'],
+      ['Y', 'F', 'C', 'R', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'A', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'K', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'AFJ');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is falsy value, LAP can be found diagonally backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'S', 'A', 'N', 'K'],
+      ['Y', 'F', 'C', 'R', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'A', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'K', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LAP');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is falsy value, KAZY can not be found", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'S', 'A', 'N', 'K'],
+      ['Y', 'F', 'C', 'R', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'A', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'K', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'KAZY');
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,93 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+ 
+  if (!letters || !word) return false;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  // console.log(horizontalJoin);
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const revHorizontalJoin = letters.map(ls => ls.reverse().join(''));
+  for (const l of revHorizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  let arrayObject = {};
+
+  for (let i = 0;  i < letters.length; i ++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      if (arrayObject[j] === undefined) {
+        arrayObject[j] = [letters[i][j]];
+      } else {
+        arrayObject[j].push(letters[i][j]);
+      }
+    }
+  }
+
+  for (const index in arrayObject) {
+    if (arrayObject[index].join('').includes(word)) return true;
+    if (arrayObject[index].reverse().join('').includes(word)) return true;
+  }
+    
+
+  let arrayObjectDiagonal = {};
+  for (let i = 0;  i < letters.length; i ++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      let smaller = i <= j ? i : j;
+      if (i === 0 || j === 0) {
+        arrayObjectDiagonal[`${i}${j}`] = [letters[i][j]];
+      } else {
+        arrayObjectDiagonal[`${i - smaller}${j - smaller}`].push(letters[i][j]);
+      }
+    }
+  }
+
+  for (const index in arrayObjectDiagonal) {
+    if (arrayObjectDiagonal[index].join('').includes(word)) return true;
+    if (arrayObjectDiagonal[index].reverse().join('').includes(word)) return true;
+  }
+
+  letters.map(l => l.reverse());
+  let arrayObjectDiagonal2 = {};
+  for (let i = 0;  i < letters.length; i ++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      let smaller = i <= j ? i : j;
+      if (i === 0 || j === 0) {
+        arrayObjectDiagonal2[`${i}${j}`] = [letters[i][j]];
+      } else {
+        arrayObjectDiagonal2[`${i - smaller}${j - smaller}`].push(letters[i][j]);
+      }
+    }
+  }
+
+  for (const index in arrayObjectDiagonal2) {
+    if (arrayObjectDiagonal2[index].join('').includes(word)) return true;
+    if (arrayObjectDiagonal2[index].reverse().join('').includes(word)) return true;
+  }
+
+  return false;
+
+};
+
+
+
+
+
+
+let arraySearch = [['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+  ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+  ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L']];
+
+console.log(arraySearch[0]);
+console.log(wordSearch(arraySearch, 'AEC'));
+
+
+
+
+
+module.exports = wordSearch;
